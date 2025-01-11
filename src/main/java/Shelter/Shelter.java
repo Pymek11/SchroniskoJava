735ab8d38dfada20714840358ee9a8215b1b08a1
Fisher <mateuszrybak631@gmail.com>
2025-01-11 12:01:13 +0100

    shelter

--- src/main/java/Shelter/Shelter.java
+++ src/main/java/Shelter/Shelter.java
@@ -1,5 +1,18 @@
 package Shelter;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Shelter {
+    // Atrybuty
+    private List<Animal> animalList;
+    private List<Employee> employeeList;
+    private List<Adoption> adoptionList;
 
+    // Konstruktor
+    public Shelter() {
+        this.animalList = new ArrayList<>();
+        this.employeeList = new ArrayList<>();
+        this.adoptionList = new ArrayList<>();
+    }
 }
+
