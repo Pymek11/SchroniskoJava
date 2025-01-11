2b8b8ed9532ecf988d9bc6b377622fab10478ce4
Fisher <mateuszrybak631@gmail.com>
2025-01-11 12:15:41 +0100

    Shelter - gettery list

--- src/main/java/Shelter/Shelter.java
+++ src/main/java/Shelter/Shelter.java
@@ -13,6 +13,45 @@ public class Shelter {
         this.animalList = new ArrayList<>();
         this.employeeList = new ArrayList<>();
         this.adoptionList = new ArrayList<>();
+    };
+    public Animal getAnimalListElem(int i){
+        try {
+            if(i<animalList.size()){
+                return animalList.get(i);
+            }else{
+                throw  new IndexOutOfBoundsException();
+            }
+
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Nie ma takiego elementu!!!");
+        }
+        return null;
+    }
+    public Employee getEmployeeListElem(int i){
+        try {
+            if(i<employeeList.size()){
+                return employeeList.get(i);
+            }else{
+                throw  new IndexOutOfBoundsException();
+            }
+
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Nie ma takiego elementu!!!");
+        }
+        return null;
+    }
+    public Adoption getAdoptionListElem(int i){
+        try {
+            if(i<adoptionList.size()){
+                return adoptionList.get(i);
+            }else{
+                throw  new IndexOutOfBoundsException();
+            }
+
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Nie ma takiego elementu!!!");
+        }
+        return null;
     }
 }
 
