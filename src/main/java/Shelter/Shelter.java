0548ba7c2581c843f9ed2b1b6ca292560d7112ab
Fisher <mateuszrybak631@gmail.com>
2025-01-11 12:39:40 +0100

    shleter, zapis do pliku,

--- src/main/java/Shelter/Shelter.java
+++ src/main/java/Shelter/Shelter.java
@@ -1,6 +1,8 @@
 package Shelter;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Shelter {
     // Atrybuty
@@ -66,5 +68,55 @@ public class Shelter {
     public void addAdoptionApplicationToList(Adoption adoption){
         adoptionList.add(adoption);
     }
+
+    // Zapis zwierząt do pliku csv
+    public void saveAnimalListToCSV(String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("ID,Species,Breed,Age,Weight,Description,Picture\n"); // Nagłówki kolumn
+            for (Animal animal : animalList) {
+                writer.write(animal.getID() + "," +
+                        animal.getSpecies() + "," +
+                        animal.getBreed() + "," +
+                        animal.getAge() + "," +
+                        animal.getWeight() + "," +
+                        animal.getDescription() + "," +
+                        animal.getPicture() + "\n");
+            }
+            System.out.println("Lista zwierząt została zapisana do pliku: " + filename);
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd podczas zapisu do pliku CSV.");
+            e.printStackTrace();
+        }
+    }
+
+    // zapisz
+    public void saveEmployeeListToCSV(String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("ID,Name,Position,Salary\n"); // Nagłówki kolumn
+            for (Employee employee : employeeList) {
+                // to do
+            }
+            System.out.println("Lista pracowników została zapisana do pliku: " + filename);
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd podczas zapisu do pliku CSV.");
+            e.printStackTrace();
+        }
+    }
+
+    // zapis adopcji do csv
+    public void saveAdoptionListToCSV(String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("AdoptionID,AnimalID,EmployeeID,Date\n"); // Nagłówki kolumn
+            for (Adoption adoption : adoptionList) {
+                // to do
+            }
+            System.out.println("Lista adopcji została zapisana do pliku: " + filename);
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd podczas zapisu do pliku CSV.");
+            e.printStackTrace();
+        }
+    }
+
+
 }
 
